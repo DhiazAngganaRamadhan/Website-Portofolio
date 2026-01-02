63a9570b9e508c25387182206f65294dff287845
Dhiaz Anggana Ramadhan <dhiazanggana3110@gmail.com>
2026-01-02 18:44:59 +0700

    Update portofolio

--- js/script.js
+++ js/script.js
@@ -1,7 +1,19 @@
 function openModal(id) {
-    document.getElementById(id).style.display = "block";
-  }
-  
-  function closeModal(id) {
-    document.getElementById(id).style.display = "none";
-  }
+  document.getElementById(id).style.display = "flex";
+}
+
+function closeModal(id) {
+  const modal = document.getElementById(id);
+  modal.style.display = "none";
+
+  const iframe = modal.querySelector("iframe");
+  if (iframe) iframe.src = "";
+}
+
+function openCertModal(modalId, pdfPath) {
+  const modal = document.getElementById(modalId);
+  const iframe = modal.querySelector("iframe");
+
+  iframe.src = pdfPath;
+  modal.style.display = "flex";
+}
